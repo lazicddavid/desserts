ab12206a571d881a3af27759d23323e29d1f70ea
David Lazic <lazic.ddavid@gmail.com>
2026-04-16 23:00:13 +0200

    Added product.js

---
+++ products.js
@@ -0,0 +1,46 @@
+const products = [
+  {
+    id: 1,
+    category: "Crème Brûlée",
+    image: "img5.jpg",
+    name: "Vanilla Bean Crème Brûlée",
+    price: 7.0,
+  },
+  {
+    id: 2,
+    category: "Macaron",
+    image: "img1.jpg",
+    name: "Macaron Mix of Five",
+    price: 8.0,
+  },
+  {
+    id: 3,
+    category: "Tiramisu",
+    image: "img2.jpg",
+    name: "Classic Tiramisu",
+    price: 5.5,
+  },
+  {
+    id: 4,
+    category: "Baklava",
+    image: "img3.jpg",
+    name: "Pistachio Baklava",
+    price: 4.0,
+  },
+  {
+    id: 5,
+    category: "Pie",
+    image: "img4.jpg",
+    name: "Lemon Meringue Pie",
+    price: 5.0,
+  },
+  {
+    id: 6,
+    category: "Waffle",
+    image: "img6.jpg",
+    name: "Waffle with Berries",
+    price: 6.5,
+  },
+];
+
+export default products;
